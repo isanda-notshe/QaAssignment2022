2cc4583c016cb81e60a92b5bcc0681873f76209f
Isanda <isanda.notshe@ardoq.com>
2022-05-09 10:55:14 +0200

    Automated test cases

--- cypress/integration/assignment.js
+++ cypress/integration/assignment.js
@@ -3,6 +3,8 @@
 const username = Cypress.env('username');
 const email = Cypress.env('email');
 const password = Cypress.env('password');
+const product1 = 'Green Side Placket Detail T-Shirt'
+const product2 = 'Fancy Green Top'
 
 describe('testAssignment2022', function(){
 
@@ -13,16 +15,40 @@ cy.enterAccountInformationSignUp('Mrs',password, '2', '12', '2018');
 cy.enterAddressInformationSingUp(username, 'Notshe', 'Ardoq', 'Rodeo drive 666', 'United States', 'California', 'Los Angeles', '90210', '6210')
 cy.verifySuccessfulAccountCreation();
 cy.clickContinueAfterAccountCreation();
-// cy.deleteAccountApi('Mimi@gmail.com','bluewaterbay6210')
 });
 
 it('loginInvalid', ()=>{
     cy.logoutAccount();
     cy.signUpLogInClick();
+    cy.login('invalidEmail@gmail.com', 'invalidPassowrd');
+    cy.verifyInvalidCredentialsMessage();
 });
 
 it('endToEndPurchase', ()=>{
-
+    cy.signUpLogInClick();
+    cy.login(email, password);
+    //Add green side t shirt
+    cy.navigateToProductPage();
+    cy.searchForProduct(product1);
+    cy.verifyPrice('Rs. 1000')
+    cy.viewProductClick();
+    cy.addToBaskedClick();
+    cy.continueShoppingClick();
+    //Add fancy green top
+    cy.navigateToProductPage();
+    cy.searchForProduct(product2);
+    cy.viewProductClick();
+    cy.addToBaskedClick();
+    cy.viewCartClickModal();
+    //Verify cart items
+    cy.verifyCartItems(product1);
+    cy.verifyCartItems(product2);
+    //Checkout
+    cy.proceedToCheckoutClick();
+    cy.placeOrder();
+    //Banking details
+    cy.fillOutBankingDetails(username, '6210', '123', '12', '2077');
+    cy.payAndConfirmOrderClick();
 });
 
 })
--- cypress/support/assignmentHelper.js
+++ cypress/support/assignmentHelper.js
@@ -61,3 +61,72 @@ Cypress.Commands.add('deleteAccountApi', (email, password)=> {
 Cypress.Commands.add('logoutAccount', ()=>{
     cy.get('[href="/logout"]').click();
 });
+
+Cypress.Commands.add('login', (email,password)=>{
+    cy.get('[data-qa="login-email"]').clear().type(email);
+    cy.get('[type="password"]').clear().type(password);
+    cy.get('[data-qa="login-button"]').click();
+});
+
+Cypress.Commands.add('verifyInvalidCredentialsMessage', ()=>{
+    cy.contains('Your email or password is incorrect!').should('be.visible');
+});
+
+Cypress.Commands.add('navigateToProductPage', ()=>{
+    cy.get('[href="/products"]').click()
+});
+
+Cypress.Commands.add('searchForProduct', productName =>{
+    cy.get('[id="search_product"]').type(productName);
+    cy.get('[id="submit_search"]').click();
+});
+
+Cypress.Commands.add('verifyPrice', price => {
+    cy.contains(price).should('be.visible');
+});
+
+Cypress.Commands.add('viewProductClick', ()=>{
+    //cy.get('[href="/product_details/29"]').click();
+    cy.contains('View Product').click();
+});
+
+Cypress.Commands.add('addToBaskedClick', ()=>{
+    cy.get('[class="btn btn-default cart"]').click()
+});
+
+Cypress.Commands.add('continueShoppingClick', ()=>{
+    cy.get('[class="btn btn-success close-modal btn-block"]').click();
+});
+
+Cypress.Commands.add('viewCartClickModal', ()=>{
+    cy.get('[href="/view_cart"]:contains("View Cart")').click();
+});
+
+Cypress.Commands.add('verifyCartItems', productName =>{
+    cy.get('#cart_info').contains(productName).should('be.visible');
+});
+
+Cypress.Commands.add('proceedToCheckoutClick', ()=>{
+    cy.get('[class="btn btn-default check_out"]').click();
+});
+
+Cypress.Commands.add('placeOrder', ()=>{
+    cy.get('[class="btn btn-default check_out"]').click();
+});
+
+Cypress.Commands.add('fillOutBankingDetails', (nameOnBankCard, cardNumber, cvc, month, year)=>{
+    cy.get('[data-qa="name-on-card"]').type(nameOnBankCard);
+    cy.get('[data-qa="card-number"]').type(cardNumber);
+    cy.get('[data-qa="cvc"]').type(cvc);
+    cy.get('[data-qa="expiry-month"]').type(month);
+    cy.get('[data-qa="expiry-year"]').type(year)
+});
+
+Cypress.Commands.add('payAndConfirmOrderClick', ()=>{
+    cy.get('[id="submit"]').click();
+});
+
+Cypress.Commands.add('verifyOrderHasBeenPlaced', ()=>{
+    cy.get('[data-qa="order-placed"]').contains('Order Placed!').should('be.visible');
+    cy.contains('Congratulations! Your order has been confirmed!').should('be.visible');
+});
